fbe58f544293db51d145f7e1a2a728b30d4a19ce
Andrei Stepanenko <ftvkyo@gmail.com>
2022-02-18 17:19:03 +0000

    Added quitting with ESC and fullscreen F toggle

--- Main.gd
+++ Main.gd
@@ -3,3 +3,11 @@ extends Node
 
 func _ready():
 	randomize()
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		OS.set_window_fullscreen(!OS.window_fullscreen)
+
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
