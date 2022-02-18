b9de624d0fc3e4f024c43a5bd9e6d211c26a6be4
Andrei Stepanenko <ftvkyo@gmail.com>
2022-02-18 18:24:49 +0000

    Add camera zoom and reset

--- Main.gd
+++ Main.gd
@@ -24,6 +24,9 @@ func _ready():
 
 
 func _process(delta):
+	if Input.is_action_just_pressed("ui_reset"):
+		$PlayerCamera.reset(bubbles[current_bubble].position)
+
 	if Input.is_action_just_pressed("toggle_fullscreen"):
 		OS.set_window_fullscreen(!OS.window_fullscreen)
 
--- PlayerCamera.gd
+++ PlayerCamera.gd
@@ -2,6 +2,9 @@ extends Camera2D
 
 
 export var CameraSpeed = 1000
+export var ZoomSpeed = 2
+export var ZoomClosest = 0.25
+export var ZoomFurthest= 5
 
 
 func _ready():
@@ -17,3 +20,16 @@ func _process(delta):
 		position.x -= CameraSpeed * delta
 	if Input.is_action_pressed("ui_right"):
 		position.x += CameraSpeed * delta
+
+	if Input.is_action_pressed("ui_zoom_in"):
+		zoom *= 1 - ZoomSpeed * delta
+	if Input.is_action_pressed("ui_zoom_out"):
+		zoom *= 1 + ZoomSpeed * delta
+
+	zoom.x = clamp(zoom.x, ZoomClosest, ZoomFurthest)
+	zoom.y = clamp(zoom.y, ZoomClosest, ZoomFurthest)
+
+
+func reset(pos):
+	position = pos
+	zoom = Vector2.ONE
